f98e6c0d5bda31159d6af07767cfc80071146fc3
Dominic LoBue <dominic.lobue@gmail.com>
2020-08-27 12:54:24 -0700

    Source asdf java plugin helper that sets java env

--- envmanager.zsh
+++ envmanager.zsh
@@ -82,6 +82,7 @@ function load-asdf {
         unset -f asdf
         local asdf_script=${HOME}/.asdf/asdf.sh
         [ -s "$asdf_script" ] && . "$asdf_script"
+        . ~/.asdf/plugins/java/set-java-home.zsh
         export ASDF_LOADED=1
     fi
 }
