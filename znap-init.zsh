25b73682ecf5fa2812e7658f1d5cfa692bedb8ba
Dominic LoBue <dominic.lobue@gmail.com>
2021-08-25 16:17:48 -0700

    create plugins dir if missing

--- znap-init.zsh
+++ znap-init.zsh
@@ -3,6 +3,10 @@
 if [[ ! -d ~/.znap ]]; then
     git clone git@github.com:marlonrichert/zsh-snap.git ~/.znap
 fi
+# Check if plugins dir exists
+if [[ ! -d ~/.zsh/plugins ]]; then
+    mkdir -p ~/.zsh/plugins
+fi
 
 zstyle ':znap:*' repos-dir ~/.zsh/plugins
 source ~/.znap/znap.zsh
